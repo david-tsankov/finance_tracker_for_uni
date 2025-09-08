6d73cc155525a5afa8f190ead5bc0100454b6858
david-tsankov <david.tsankov@gmail.com>
2025-09-08 15:07:02 +0300

    added balance and transactions dictionary, need to find way to save transactions

--- financial_modules/transactions.py
+++ financial_modules/transactions.py
@@ -1,17 +1,37 @@
 from datetime import datetime
+
 class Transaction:
-    def __init__(self,amount: float=0,category: str="General",payment_method: str="Cash",date: str=datetime.today(),note: str=""):
+    balance=0
+    transactions={
+
+    }
+    index=0
+    def __init__(self,amount: float=0,category: str="General",payment_method: str="Cash",note: str="",date: str=datetime.today()):
         self.amount=amount
         self.category=category
         self.payment_method=payment_method
         self.date=date
         self.note=note
+        Transaction.transaction_saver(self)
+        Transaction.balance+=self.amount
 
     def __str__(self):
         if self.amount>0:
-            return f"Transaction details\nType: Income\nAmount: {self.amount}\nCategory: {self.category}\nPayment method: {self.payment_method}\nDate: {self.date}\nNote: {self.note}"
+            return f"Type: Income\nAmount: {self.amount}\nCategory: {self.category}\nPayment method: {self.payment_method}\nDate: {self.date}\nNote: {self.note}"
         elif self.amount<0:
-            return f"Transaction details\nType: Expense\nAmount: {self.amount}\nCategory: {self.category}\nPayment method: {self.payment_method}\nDate: {self.date}\nNote: {self.note}"
+            return f"Type: Expense\nAmount: {self.amount}\nCategory: {self.category}\nPayment method: {self.payment_method}\nDate: {self.date}\nNote: {self.note}"
+    
+    def transaction_saver(self):
+        Transaction.transactions[Transaction.index]=self
+        Transaction.index+=1
+
+    def list_transactions():
+        for key,transaction in Transaction.transactions.items():
+            print(f"{key}=\n{transaction}\n")
+
+    def show_balance():
+        print(f"Balance={Transaction.balance}")
+
 
 
 
@@ -19,5 +39,7 @@ class Transaction:
 
 
 if __name__=="__main__":
-    Rent=Transaction(-500,"Parents money","Cash")
-    print(Rent)
+    Rent=Transaction(-500,"Rent","Cash","Parents money")
+    Salary=Transaction(1000,"Salary","Cash")
+    Transaction.list_transactions()
+    Transaction.show_balance()
